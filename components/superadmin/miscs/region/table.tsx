d2cf6ca9b1cdc9f5b9dc39292abf770432cb45fb
lokeshbaj <lokesh@coveredpress.com>
2022-09-10 15:46:29 +0545

    admin dashboard home

--- components/superadmin/miscs/region/table.tsx
+++ components/superadmin/miscs/region/table.tsx
@@ -258,20 +258,4 @@ const RegionList: React.FC = () => {
   );
 };
 
-
-function beforeUpload(file: RcFile) {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
-    return Upload.LIST_IGNORE;
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-    return Upload.LIST_IGNORE;
-  }
-  return isJpgOrPng && isLt2M;
-};
-
-
 export default RegionList;
